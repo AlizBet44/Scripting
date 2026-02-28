62e9b2483ee385b7fef9bbba191134592723385e
Alison Betancourt <134250270+AlizBet44@users.noreply.github.com>
2026-02-28 12:31:26 -0500

    Add files via upload
    Clase de prueba Unity M3L1 (Scripts)

---
+++ HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Voy a imprimir el hola mundo
+        print("Hola mundo");
+        print(5);
+        print(5 + 9);
+        print("5 - 2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
+
